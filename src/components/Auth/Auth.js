3c2679783d89c7887b01f88f37d6909c0915b98c
Andrew Nicholas <andrew@vyg.co.nz>
2019-12-19 14:44:05 +1300

    update auth changes

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -18,19 +18,26 @@ const Auth = () => {
       {({ location, navigate }) =>
         location.search ? (
           <div>
-            <h3>Code: {getCode(queryString.parse(location.search))} </h3>
+            <h3>
+              Authorisation Code: {getCode(queryString.parse(location.search))}{" "}
+            </h3>
             <h4>
-              Next Step:{" "}
-              <a href="https://developers.strava.com/docs/getting-started/">
-                Get the refresh token and add to environment variables
-              </a>
+              Next Step: Using a http client with POST capabilities get your
+              refresh token using the following URL
             </h4>
+            <p>{`https://www.strava.com/oauth/token?client_secret=[client_secret_id]&code=${getCode(
+              queryString.parse(location.search)
+            )}0&grant_type=authorization_code&client_id=${clientID}`}</p>
+
+            <a href="https://developers.strava.com/docs/getting-started/">
+              Example here
+            </a>
           </div>
         ) : (
           <a
             href={`http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=${responseType}&redirect_uri=${redirectUri}&approval_prompt=force&scope=${scope}`}
           >
-            Get auth code
+            Get you auth code
           </a>
         )
       }
